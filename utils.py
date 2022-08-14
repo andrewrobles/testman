766b6d2bec49a2b0172c46f59cefe3be7e98c905
Andrew Robles <andrewrobles@verizon.net>
2022-08-14 16:11:00 -0700

    Move browser logic to bot contructor

--- utils.py
+++ utils.py
@@ -3,14 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 from bs4 import BeautifulSoup
+import configparser
 
-class InstagramBot:
-    def __init__(self, browser):
+import json
+
+class Bot:
+    def __init__(self):
+        file = open('settings.json', 'r')
+        settings = json.load(file)
+        file.close()
+
+        username = settings['username']
+        password = settings['password']
+
+        browser = webdriver.Firefox()
         self._browser = browser
         self._browser.implicitly_wait(10)
         self._base_url = 'https://www.leetcode.com/accounts/login/'
         self._browser.get(self._base_url)
         sleep(1)
+        self.login(username, password)
 
     def login(self, username, password):
         username_input = self._browser.find_element_by_css_selector("input[name='login']")
@@ -22,6 +34,9 @@ class InstagramBot:
         login_button = self._browser.find_element_by_xpath("//button[@id='signin_btn']")
         login_button.click()
         sleep(5)  
+
+    def load_amazon_questions(self):
+        self._browser.get('https://leetcode.com/company/amazon')
 
     def _find_element_by_text(self, text):
         return self._browser.find_element_by_xpath("//*[text()='{}']".format(text)) 
