3d3a6fd70cf834272f06309af7a7672d9a238f32
Andrew Robles <andrewrobles@verizon.net>
2022-08-14 15:57:02 -0700

    Implement login

--- utils.py
+++ utils.py
@@ -7,19 +7,19 @@ from bs4 import BeautifulSoup
 class InstagramBot:
     def __init__(self, browser):
         self._browser = browser
-        self._browser.implicitly_wait(60)
-        self._base_url = 'https://www.instagram.com'
+        self._browser.implicitly_wait(10)
+        self._base_url = 'https://www.leetcode.com/accounts/login/'
         self._browser.get(self._base_url)
         sleep(1)
 
     def login(self, username, password):
-        username_input = self._browser.find_element_by_css_selector("input[name='username']")
+        username_input = self._browser.find_element_by_css_selector("input[name='login']")
         password_input = self._browser.find_element_by_css_selector("input[name='password']")
 
         username_input.send_keys(username)
         password_input.send_keys(password)
-
-        login_button = self._browser.find_element_by_xpath("//button[@type='submit']")
+        sleep(1)
+        login_button = self._browser.find_element_by_xpath("//button[@id='signin_btn']")
         login_button.click()
         sleep(5)  
 
